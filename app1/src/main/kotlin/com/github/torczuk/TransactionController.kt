b93a4d5778693112431e49663fbef898b4a7979c
Arek Torczuk <torczuk@gmail.com>
2019-04-14 01:10:42 +0800

    Introduce endpoints for getting transaction status

---
+++ app1/src/main/kotlin/com/github/torczuk/TransactionController.kt
@@ -0,0 +1,21 @@
+package com.github.torczuk
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["api/v1/transaction"])
+class TransactionController {
+
+    @PostMapping(path = ["/{id}"])
+    fun processTransaction(@PathVariable("id") transactionId: String): ResponseEntity<String> {
+        val location = """{"location": "/api/v1/transaction/$transactionId"}"""
+        return ResponseEntity.accepted().body(location)
+    }
+
+    @GetMapping(path = ["/{id}"])
+    fun transactionStatus(@PathVariable("id") transactionId: String): ResponseEntity<String> {
+        val status = """{"status": "in progress"}"""
+        return ResponseEntity.ok(status)
+    }
+}
---
+++ app1/src/test/kotlin/com.github.torczuk/TransactionControllerTest.kt
@@ -0,0 +1,48 @@
+package com.github.torczuk
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus.ACCEPTED
+import org.springframework.http.HttpStatus.OK
+import java.util.*
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+internal class TransactionControllerTest(
+        @Autowired private val restTemplate: TestRestTemplate,
+        @LocalServerPort private val randomServerPort: Int) {
+
+    @Test
+    fun shouldAcceptTransactionToBeProcessedAndResponseWithStatusLocation() {
+        val transaction = UUID.randomUUID().toString()
+
+        val response = POST("/api/v1/transaction/$transaction")
+
+        assertThat(response.statusCode).isEqualTo(ACCEPTED)
+        assertThat(response.body).isEqualTo("""{"location": "/api/v1/transaction/$transaction"}""".trimIndent())
+    }
+
+    @Test
+    fun shouldResponseTransactionStatus() {
+        val transaction = UUID.randomUUID().toString()
+
+        val response = GET("/api/v1/transaction/$transaction")
+
+        assertThat(response.statusCode).isEqualTo(OK)
+        assertThat(response.body).isEqualTo("""{"status": "in progress"}""".trimIndent())
+    }
+
+    private fun POST(uri: String) = restTemplate.postForEntity<String>(
+            "http://localhost:$randomServerPort/$uri",
+            null,
+            String::class.java)
+
+
+    private fun GET(uri: String) = restTemplate.getForEntity(
+            "http://localhost:$randomServerPort/$uri",
+            String::class.java)
+}
