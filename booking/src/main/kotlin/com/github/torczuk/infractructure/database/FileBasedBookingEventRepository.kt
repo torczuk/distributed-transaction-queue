f1262494bbcc33bcbe66a5861f988af1b936cbd7
Arek Torczuk <torczuk@gmail.com>
2019-04-25 00:37:36 +0800

    Added vintage junit for temporary folder support in tests

--- booking/src/main/kotlin/com/github/torczuk/infractructure/database/FileBasedBookingEventRepository.kt
+++ booking/src/main/kotlin/com/github/torczuk/infractructure/database/FileBasedBookingEventRepository.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.torczuk.domain.BookingEvent
 import com.github.torczuk.domain.BookingEventRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
@@ -18,14 +19,17 @@ class FileBasedBookingEventRepository(
         @Value("\${application.file-based-db.path}") path: String,
         @Autowired val mapper: ObjectMapper) : BookingEventRepository {
 
+    val log = LoggerFactory.getLogger(FileBasedBookingEventRepository::class.java)
     val location = Paths.get(path)
 
     override fun exist(transactionId: String): Boolean {
+        log.debug("exist $transactionId")
         return Files.list(location)
                 .anyMatch { path -> path.fileName.toFile().name.startsWith(transactionId) }
     }
 
     override fun findAll(): List<BookingEvent> {
+        log.debug("findAll")
         return Files.list(location)
                 .map { path -> Files.readAllBytes(path)  }
                 .map { bytes -> String(bytes) }
@@ -34,6 +38,7 @@ class FileBasedBookingEventRepository(
     }
 
     override fun findBy(transactionId: String): List<BookingEvent> {
+        log.debug("findBy $transactionId")
         return Files.list(location)
                 .filter { file -> file.fileName.toFile().name.startsWith(transactionId) }
                 .map { file -> Files.readAllBytes(file)  }
@@ -43,6 +48,7 @@ class FileBasedBookingEventRepository(
     }
 
     override fun save(event: BookingEvent) {
+        log.debug("save $event")
         Files.write(location.resolve("${event.transaction}_${event.type}"),
                 mapper.writeValueAsString(event).toByteArray(Charsets.UTF_8),
                 StandardOpenOption.CREATE)
